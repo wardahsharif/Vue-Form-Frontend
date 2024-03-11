b2f558ce4c0b9330b2b50d0ceaf9dd3f6076c4ff
gitrevision <119267583+wardahsharif@users.noreply.github.com>
2024-03-11 17:58:54 +0300

    created folder

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from 'vue';
+
+function increment() {
+count.value++
+}
+
+
+const count = ref(0)
+
+</script>
+
+
+<template>
+<h1>Counter App</h1>
+<button @click="increment">Increment</button>
+<p>count: {{count}}</p>
+</template>
+ 
