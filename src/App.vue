09932e0e843ff18b50d4abaf48c5bff2966bf5fe
gitrevision <119267583+wardahsharif@users.noreply.github.com>
2024-03-12 17:31:33 +0300

    added router and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <script setup>
-import {ref} from 'vue';
-
-function increment() {
-count.value++
-}
-
-
-const count = ref(0)
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 </script>
-
-
 <template>
-<h1>Counter App</h1>
-<button @click="increment">Increment</button>
-<p>count: {{count}}</p>
+  <div id="app">
+ <ul>
+    
+<li> <router-link to="/">Home</router-link> </li>
+ <li> <router-link to="/login">Login</router-link> </li> 
+ <router-view></router-view> 
+ </ul>
+  </div>
 </template>
- 
+
+<style scoped>
+ul {
+   
+    margin: 0 auto;
+    padding: 0;
+    list-style-type: none;
+    margin-bottom: 10px;
+}
+
+li {
+    display: inline;
+    padding: 5px;
+    margin: 30px;
+    
+}
+
+</style>
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Login</h1>
+<label>Username</label><br>
+<input type="text"/><br>
+<label>Password</label><br>
+<input type="password"/>
+</template>
