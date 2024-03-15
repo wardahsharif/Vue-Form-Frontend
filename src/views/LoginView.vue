537157f9196863d550a5cecfc2c2a4cd3bc87fe8
gitrevision <119267583+wardahsharif@users.noreply.github.com>
2024-03-15 17:04:20 +0300

    added form to my login component

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+     <div class="signup-form container border rounded mt-5">
+        <h1 class="text-center mt-5">Login</h1>
+
+      <form @submit.prevent="loginUser">
+   
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" value="email"  v-model="formData.email">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" value="password" v-model="formData.password">
+  </div>
+  <button type="submit" class="btn btn-secondary mb-5">Submit</button>
+   <a href="#" @click.prevent="goToForgotPassword" class="forgot-password-link">Forgot Password?</a>
+</form>
+  
+    </div>
+</template>
